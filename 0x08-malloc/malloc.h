573efa97a9405d2833c265040d84a273d518aa59
sravanthisinha <sravani.sinha@gmail.com>
2017-12-10 16:48:20 -0800

    task 1

--- 0x08-malloc/malloc.h
+++ 0x08-malloc/malloc.h
@@ -13,11 +13,15 @@ typedef struct s_block *t_block;
 * struct s_block - Holds the details of each block
 * @size: size of block
 * @next: pointer to next block
+* @free: if block is free
+* @data: data to be hold
 */
 struct s_block
 {
 	size_t size;
 	struct s_block *next;
+	int free;
+	char data[1];
 };
 
 #define BLOCK_SIZE sizeof(struct s_block)
